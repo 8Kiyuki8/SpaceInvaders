ac6fd7d63715eda52a55d4214a2cf90c9ffd5ff8
Wesdell <wesdell17@gmail.com>
2024-07-03 22:12:35 -0500

    Corregir nombre de recursos y eliminar codigos de prueba colaboracion de Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("1005444912");
-    System.out.println("4");
-    System.out.println(" >_< ");
-    System.out.println("Hola, soy Anna");
   }
 }
