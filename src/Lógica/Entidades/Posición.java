34980a72af68889a313c2858de821c81053e16b2
FrancissC11 <franciss.cartagena@epn.edu.ec>
2024-07-22 18:30:48 -0500

    Implementar clase posición

---
+++ src/Lógica/Entidades/Posición.java
@@ -0,0 +1,28 @@
+package Lógica.Entidades;
+
+public class Posición {
+
+  private int posiciónX;
+  private int posiciónY;
+
+  public Posición(int x, int y) {
+    posiciónX = x;
+    posiciónY = y;
+  }
+
+  public int obtenerPosiciónX() {
+    return posiciónX;
+  }
+
+  public void establecerPosiciónX(int nuevaPosiciónX) {
+    posiciónX = nuevaPosiciónX;
+  }
+
+  public int obtenerPosiciónY() {
+    return posiciónY;
+  }
+
+  public void establecerPosiciónY(int nuevaPosiciónY) {
+    posiciónY = nuevaPosiciónY;
+  }
+}
